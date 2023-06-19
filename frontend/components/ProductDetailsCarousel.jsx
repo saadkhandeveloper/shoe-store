6883e5edba00a09a51d20fa4c23dfd8d889b629e
saadkhandeveloper <131966611+saadkhandeveloper@users.noreply.github.com>
2023-06-18 20:32:44 -0700

    first change

---
+++ frontend/components/ProductDetailsCarousel.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+
+const ProductDetailsCarousel = () => {
+  return (
+    <div className="text-white text-[20px] w-full max-w-[1360px] mx-auto sticky top-[50px]"></div>
+  );
+};
+
+export default ProductDetailsCarousel;
